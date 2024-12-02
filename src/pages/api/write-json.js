c6b029780b3fcfc4826e4de95a6968432c48b75b
madsab <mads.barnes@hotmail.com>
2024-12-02 08:47:47 +0100

    remove fs

--- src/pages/api/write-json.js
+++ src/pages/api/write-json.js
@@ -1,12 +1,10 @@
-import fs from 'fs';
+
 
 export async function POST({ request }) {
   try {
     const data = await request.json(); // Get data from the POST request
     data.details = data.details.trim(); // Remove leading and trailing spaces
-    const jsonData = JSON.stringify(data, null, 2);
     // Write the JSON file (this works only on the server side)
-    fs.writeFileSync('/public/data.json', jsonData); // Save in the public folder
 
     return new Response(JSON.stringify({ success: true }), {
       status: 200,
