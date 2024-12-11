cd466abd29b035aefd49fb7065327804638c9d16
madsab <mads.barnes@hotmail.com>
2024-12-11 12:39:48 +0100

    change to ts, add door 14-19

---
+++ src/data/XmasDoors.ts
@@ -0,0 +1,148 @@
+export const XmasDoors = [
+    {
+        id: "uh89qwiudiqh09201393n13928n9128912",
+        num: 18,
+        content: "🔨🎄🥤",
+        details: "Nam, nam, nam! Alltid best på glass"
+    },
+    {
+        id: "a1b2c3d4e5f6g7ah8i9j0aa2m3n4o5p6q",
+        num: 5,
+        content: "Movie Night 🎬",
+        details: "Gjør klar øynene for vi skal på Kino!. Du har fått to billetter til Red One kl 18:00 på Nova kino. God fornøyelse (Ikke boutiqe denne gangen😢)!"
+    },
+    {
+        id: "r7s8t9u0v1w2x3y4z5aasdsa7c8d9e0f1g2h",
+        num: 24,
+        content: "🕯️",
+        details: "Ta en titt i kjøleskapet for en godbit!"
+    },
+    {
+        id: "i3j4k5l6m7n8o9p0q1asdkt4u5v6w7x8y",
+        num: 12,
+        content: "Vestlandet? ",
+        details: "Jeg kan sverge på at denne har vært før? Ta en titt i kjøleskapet for en deilig godbit!"
+    },
+    {
+        id: "z9a0b1c2d3e4f5g6h7i8easji1m2n3o4p",
+        num: 1,
+        content: "Siste Sushi 🍣",
+        details: "Du har fått en gratis sushi buffet på Sabrura. Gjelder kun i dag. Dette blir nok den siste sushien før julematen kommer."
+    },
+    {
+        id: "q5r6s7t8u9v0w1x2y3ejahshde9f0g",
+        num: 20,
+        content: "🎅",
+        details: "Ta en titt i kjøleskapet for en godbit!"
+    },
+    {
+        id: "h1i2j3k4l5m6n7o8p9cvnashje3u4v5w6x",
+        num: 9,
+        content: "Om du ikke kan dra til Vestlander, så kommer Vestlander til deg",
+        details: "Ta en titt i kjøleskapet for en godbit du kan ha vedsiden av lesingen"
+    },
+    {
+        id: "y7z8a9b0c1d2e3f4g5h6iasdmksdc0m1n2o",
+        num: 22,
+        content: "🦌",
+        details: "Ta en titt i kjøleskapet for en godbit!"
+    },
+    {
+        id: "p3q4r5s6t7u8v9eoauensaa4b5c6d7e8f",
+        num: 3,
+        content: "Litt leske til starten av eksamensperioden 🧋",
+        details: "Ta en titt i kjøleskapet!"
+    },
+    {
+        id: "g9h0i1j2k3l4m5n6o7p8aehasbt2u3v4w",
+        num: 14,
+        content: "Fish n ... 🍟",
+        details: "Er det smak av fisk? Er det smak av chips? Nei, det er jo smak av Ribbe! Hvor kan det ligge da?"
+    },
+    {
+        id: "x5y6z7a8b9c0d1e2f3gvcbhjek8l9m0n",
+        num: 8,
+        content: "Don't worry baby, breakfast is on me 🥐",
+        details: "Velkommen til Bårnes bakeri. Hva kan vi friste med?"
+    },
+    {
+        id: "o1p2q3r4s5t6u7v8w9x0y1asdjkoja4c5d6e",
+        num: 19,
+        content: "Julemarked 2.0 🎄",
+        details: "Ingen overraskelse at i dag går turen til Jul i Vinterland!! Husk å ta med kritikeren i deg, det skal smakes på masse digg!"
+    },
+    {
+        id: "f7g8h9i0j1k2l3m4n5o6amkasndois0t1u2v",
+        num: 4,
+        content: "En søt overraskelse 🍬",
+        details: "Ta en titt i skapet! Mitt skap ofc😘"
+    },
+    {
+        id: "w3x4y5z6a7b8c9d0e1f2g3asdkasjoie8m",
+        num: 23,
+        content: "🔔",
+        details: "Ta en titt i kjøleskapet for en godbit!"
+    },
+    {
+        id: "n9o0p1q2r3s4t5u6v7w8acmoashi2b3c4d",
+        num: 6,
+        content: "1/4 av veien er gjennomført 🎉. Det fortjener liten something",
+        details: "Suprise til ikveld!🤫"
+    },
+    {
+        id: "e5f6g7h8i9j0k1l2m3n4asdjajs7r8s9t0u",
+        num: 10,
+        content: "Vi blånisser låner og gir noe tilbake 🎁",
+        details: "Ta en titt i kjøleskapet for en god refreshment. Virker kjent?"
+    },
+    {
+        id: "v1w2x3y4z5a6b7c8d9e0f1g2asdoiejans6l",
+        num: 21,
+        content: "🎁",
+        details: "Ta en titt i kjøleskapet for en godbit!"
+    },
+    {
+        id: "m7n8o9p0q1r2s3t4u5v6wplajes2c",
+        num: 13,
+        content: "Et hjerte for deg ❤️",
+        details: "Kan det ligge noe i kjøleskapet i dag også da?. Elsker deg❤️"
+    },
+    {
+        id: "d3e4f5g6h7i8j9k0l1moapek6r7s8t",
+        num: 2,
+        content: "En liten hilsen 🎅",
+        details: "Uten deg hadde ikke livet vært det samme. Tusen takk for alt du gjør for meg, både det jeg ser og det som foregår bak kulissene. Jeg elsker deg❤️"
+    },
+    {
+        id: "u9v0w1x2y3z4a5b6asovman9f0g1h2i3j4k",
+        num: 7,
+        content: "Gjør det klar for en fantastisk middag 🍽️",
+        details: "I dag har fått en reservasjon for to La Bårnes. God appetitt!"
+    },
+    {
+        id: "l5m6n7o8p9q0r1s2t3u4v53askdklz9a0b",
+        num: 17,
+        content: "På en eksamensdag 📚",
+        details: "Alle trenger noe godt i kroppen for å holde konsentrasjonen oppe. Du trenger kanskje flere? Se bak gardinen."
+    },
+    {
+        id: "c1d2e3f4g5h6i7j8k9l0m1asdmas5r6s",
+        num: 11,
+        content: "Julemarked 🎄",
+        details: "I dag skal vi på Trondheim julemarked!! Husk å kle deg etter været og ta med godt julehumør."
+    },
+    {
+        id: "t7u8v9wa31ymana4b5c6d7e8f9g0h1i2j",
+        num: 16,
+        content: "Motiverende ord 📚",
+        details: "Nå er du snart i mål med eksamen. Jeg er stolt av deg og det du får til, dette nailer du. Takk for at du er min bestevenn, slik at vi kan gå gjennom alle utfordringer sammen. Kyss, klapp og klem fra din andre halvdel❤️. "
+    },
+    {
+        id: "k3l4s3n6o7p8q9r0s1t2u3v4w5x6y7z8a",
+        num: 15,
+        content: "JOHANNE IS PROUD TO PRESENT? 🏠🏴󠁧󠁢󠁥󠁮󠁧󠁿",
+        details: "'She has it all. The school, the friends, the guy. This holiday season, find out what Johanne doesn't have.' Gjør deg klar for en cozy film-kveld 'in the cottage' med den beste drikke og snacks."
+    }
+]
+
+export type Door = typeof XmasDoors[number]
